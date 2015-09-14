48423bad3df76b876581abdc0c8fa063b77244a9
chocoluffy <yushunzhe951104@gmail.com>
2015-09-13 20:23:09 -0400

    #1

--- #1-Multiples of 3 and 5.rb
+++ #1-Multiples of 3 and 5.rb
@@ -1,3 +1,10 @@
+# fail the #testcase 2 and 3 because these tests involve large numbers
+# and when for large float number converted to integer, there will be a 
+# huge rounding error!
+# 
+# solution: if we can use bitwise operation, like 18 >> 2 = 4, only if 
+# we only involve division where divisor is 2;
+
 def sum_all_multiples_ab(a, b, max)
   sum = sum_all_multiples(a, max)
   sum += sum_all_multiples(b, max)
@@ -16,4 +23,5 @@ number_of_tests = gets.to_i
 number_of_tests.times do
     p sum_all_multiples_ab(3, 5, gets.to_i).to_i
 end
+
 
