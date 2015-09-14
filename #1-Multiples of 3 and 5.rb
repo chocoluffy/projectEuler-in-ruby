40f8d2e9e0ef1dc034745f7ac015bdabb3551650
chocoluffy <yushunzhe951104@gmail.com>
2015-09-13 20:19:14 -0400

    #1

--- #1-Multiples of 3 and 5.rb
+++ #1-Multiples of 3 and 5.rb
@@ -1,4 +1,19 @@
-gets
-2.times do
-    p (1...gets.to_i).select {|num| num%3==0 || num%5==0}.reduce(0, :+)
+def sum_all_multiples_ab(a, b, max)
+  sum = sum_all_multiples(a, max)
+  sum += sum_all_multiples(b, max)
+  sum -= sum_all_multiples(a*b, max)
+  sum
 end
+
+def sum_all_multiples(n,max)
+  count = (max-1)/n
+  last = count * n
+    sum = ((n + last) / 2.to_f) * count
+  sum
+end
+
+number_of_tests = gets.to_i
+number_of_tests.times do
+    p sum_all_multiples_ab(3, 5, gets.to_i).to_i
+end
+
